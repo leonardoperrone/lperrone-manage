ff99f9369e947fa7843e881eb6609d4d559ecef2
Leonardo Perrone <liox2012@gmail.com>
2019-10-31 21:33:55 +0100

    removed rxjs from

--- frontend/ng-lperrone-manage/src/app/services/technology.service.ts
+++ frontend/ng-lperrone-manage/src/app/services/technology.service.ts
@@ -11,28 +11,28 @@ export class TechnologyService {
   }
 
   getTechnologies(): Observable<any> {
-    return from(this.http.get('http://127.0.0.1:3000/api/v1/technologies'));
+    return this.http.get('http://127.0.0.1:3000/api/v1/technologies');
   }
 
   createTechnology(value) {
-    return from(this.http.post(`http://127.0.0.1:3000/api/v1/technologies`, value, {
+    return this.http.post(`http://127.0.0.1:3000/api/v1/technologies`, value, {
       reportProgress: true,
       observe: 'events'
-    }));
+    });
   }
 
   updateTechnology(techId: string, value: any): Observable<any> {
-    return from(this.http.patch(`http://127.0.0.1:3000/api/v1/technologies/${techId}`, value, {
+    return this.http.patch(`http://127.0.0.1:3000/api/v1/technologies/${techId}`, value, {
       reportProgress: true,
       observe: 'events'
-    }));
+    });
   }
 
   deleteTechnology(techId: string): Observable<any> {
-    return from(this.http.delete(`http://127.0.0.1:3000/api/v1/technologies/${techId}`));
+    return this.http.delete(`http://127.0.0.1:3000/api/v1/technologies/${techId}`);
   }
 
   deleteLogo(techId: string, logoIndex: number): Observable<any> {
-    return from(this.http.delete(`http://127.0.0.1:3000/api/v1/technologies/${techId}/logo/${logoIndex}`));
+    return this.http.delete(`http://127.0.0.1:3000/api/v1/technologies/${techId}/logo/${logoIndex}`);
   }
 }
